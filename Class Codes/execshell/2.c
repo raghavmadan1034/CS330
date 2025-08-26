f46c01997df00b2a5a382d88a722aadbff8632b2
raghavmadan1034 <raghav.madan1034@gmail.com>
2025-08-27 03:36:46 +0530

    Class codes for lab quiz 1

---
+++ Class Codes/execshell/2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main(void)
+{
+    int i = 0;
+    exit(-1);
+    for(i=0; i<3; ++i){
+          printf("%s: mypid = %d\n", __FILE__, getpid());
+	 // sleep(1);
+    }	  
+    exit(0);
+}
---
+++ Class Codes/fileAPI/stdinout.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+   int x;
+   printf("Enter: ");
+   scanf("%d", &x);
+   fprintf(stderr, "x=%d\n", x);
+   //printf("x=%d\n", x);
+   exit(0);
+}
